338bce468df6016932773d6e0bb5fc0e500672f6
Vladimir <papa1963@hotmail.de>
2024-07-03 06:54:02 +0200

    sirius

--- src/SecondMain.java
+++ src/SecondMain.java
@@ -1,28 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class SecondMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<String> strings = new ArrayList<>();
+
         System.out.println("Введите кол.  строк");
         int namberString = Integer.parseInt(scanner.nextLine());
         System.out.println("Введите строки");
-        strings.add(String.valueOf(scanner.nextInt()));
-        List<Integer> str = strings.stream()
-                .map(strg -> strg.length())
-                .filter(integer -> integer>=15)
-                .toList();
-        System.out.println(str);
+        for (int i = 0; i < namberString; i++) {
+            strings.add(scanner.nextLine());
+        }
+        List<Integer> lengths = strings.stream()
 
 
-
-
-
-
-
+                .map(String::length)
+                .filter(length -> length >= 15)
+                .collect(Collectors.toList());
+        System.out.println(lengths);
 
     }
 
+
 }
+
+
